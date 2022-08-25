8cab36c203257c45b13700a09ddd6a27e854b349
Kishlay Kumar <kishlaykumar@iitg.ac.in>
2022-08-25 21:26:37 +0530

    initial Testing of api done

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
+const { request } = require('express');
 const express = require('express')
 const app = express()
 const port = 3000
-
-app.get('/',(req,res)=>{
-    res.status(200).send({
-        message: 'Welcome!'
-    })
+app.use(express.json())
+app.get('/', (req, res) => {
+    res.status(200).send("<h2>Just getting started</h2>")
+})
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.send("Recieved a post request")
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log('Hehe Welcome to port no. ' + port)
 })
